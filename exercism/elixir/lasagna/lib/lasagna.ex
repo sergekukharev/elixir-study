22d2863adfa40f071442169122ea517a2d30ad72
Sergei Kukhariev <serge.kukharev@gmail.com>
2024-05-10 09:59:18 -0300

    Finished `Lasagna`

--- exercism/elixir/lasagna/lib/lasagna.ex
+++ exercism/elixir/lasagna/lib/lasagna.ex
@@ -1,11 +1,18 @@
 defmodule Lasagna do
   # Please define the 'expected_minutes_in_oven/0' function
+  def expected_minutes_in_oven(), do: 40
 
   # Please define the 'remaining_minutes_in_oven/1' function
+  def remaining_minutes_in_oven(mins_in_oven), do: expected_minutes_in_oven() - mins_in_oven
 
   # Please define the 'preparation_time_in_minutes/1' function
+  def preparation_time_in_minutes(num_layers), do: num_layers * 2
 
   # Please define the 'total_time_in_minutes/2' function
+  def total_time_in_minutes(num_layers, mins_in_oven) do
+    preparation_time_in_minutes(num_layers) + mins_in_oven
+  end
 
   # Please define the 'alarm/0' function
+  def alarm(), do: "Ding!"
 end
