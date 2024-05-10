c221d2902f1cdfc84d7a8cfd389189c948a148b0
Sergei Kukhariev <serge.kukharev@gmail.com>
2024-05-10 11:52:33 -0300

    First `log_level` solution

--- exercism/elixir/log-level/lib/log_level.ex
+++ exercism/elixir/log-level/lib/log_level.ex
@@ -1,9 +1,41 @@
 defmodule LogLevel do
-  def to_label(level, legacy?) do
-    # Please implement the to_label/2 function
+  def to_label(level, true) do
+    cond do
+      level == 1 -> :debug
+      level == 2 -> :info
+      level == 3 -> :warning
+      level == 4 -> :error
+      true -> :unknown
+      nil -> :unknown
+      false -> :unknown
+    end
+  end
+
+  def to_label(level, false) do
+    cond do
+      level == 0 -> :trace
+      level == 1 -> :debug
+      level == 2 -> :info
+      level == 3 -> :warning
+      level == 4 -> :error
+      level == 5 -> :fatal
+      true -> :unknown
+      nil -> :unknown
+      false -> :unknown
+    end
   end
 
   def alert_recipient(level, legacy?) do
-    # Please implement the alert_recipient/2 function
+    code = to_label(level, legacy?)
+
+    cond do
+      code == :error -> :ops
+      code == :fatal -> :ops
+      code == :unknown -> cond do
+        legacy? == true -> :dev1
+        legacy? == false -> :dev2
+      end
+      true -> false
+    end
   end
 end
