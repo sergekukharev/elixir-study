ba6226440f8df3dcccbfa618651efc1a60dc95cf
Sergei Kukhariev <serge.kukharev@gmail.com>
2024-05-10 11:41:16 -0300

    Using matching in function arguments

--- exercism/elixir/language-list/lib/language_list.ex
+++ exercism/elixir/language-list/lib/language_list.ex
@@ -1,8 +1,8 @@
 defmodule LanguageList do
   def new(), do: []
   def add(list, language), do: [language | list]
-  def remove(list), do: tl(list)
-  def first(list), do: hd(list)
+  def remove([_ | tail]), do: tail
+  def first([head | _]), do: head
   def count(list), do: length(list)
   def functional_list?(list), do: "Elixir" in list
 end
