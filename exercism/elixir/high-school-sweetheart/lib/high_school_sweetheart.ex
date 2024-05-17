d5b616d90e72d1f0551d0d28e621473ff4653434
Sergei Kukhariev <serge.kukharev@gmail.com>
2024-05-17 14:37:52 -0300

    Implemented `high-school-sweetheart`

--- exercism/elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
+++ exercism/elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -1,32 +1,33 @@
 defmodule HighSchoolSweetheart do
   def first_letter(name) do
-    # Please implement the first_letter/1 function
+    String.trim(name) |> String.at(0)
   end
 
   def initial(name) do
-    # Please implement the initial/1 function
+    (first_letter(name) |> String.upcase()) <> "."
   end
 
   def initials(full_name) do
-    # Please implement the initials/1 function
+    [first, last | _] = full_name |> String.trim() |> String.split()
+    initial(first) <> " " <> initial(last)
   end
 
   def pair(full_name1, full_name2) do
-    #      ******       ******
-    #    **      **   **      **
-    #  **         ** **         **
-    # **            *            **
-    # **                         **
-    # **     X. X.  +  X. X.     **
-    #  **                       **
-    #    **                   **
-    #      **               **
-    #        **           **
-    #          **       **
-    #            **   **
-    #              ***
-    #               *
-
-    # Please implement the pair/2 function
+    """
+         ******       ******
+       **      **   **      **
+     **         ** **         **
+    **            *            **
+    **                         **
+    **     #{initials(full_name1)}  +  #{initials(full_name2)}     **
+     **                       **
+       **                   **
+         **               **
+           **           **
+             **       **
+               **   **
+                 ***
+                  *
+    """
   end
 end
