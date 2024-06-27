8fb87fdcd9fe431185c5d10246cdd8b727cab7c0
Sergei Kukhariev <serge.kukharev@gmail.com>
2024-06-27 18:08:25 -0300

    Can add dogs now

--- phx-projects/hello/lib/hello_web/controllers/dog_controller.ex
+++ phx-projects/hello/lib/hello_web/controllers/dog_controller.ex
@@ -4,6 +4,11 @@ defmodule HelloWeb.DogController do
   def index(conn, _) do
     json(conn, [Hello.Repo.all(Hello.Dog)])
   end
+
+  def create(conn, %{"name" => name, "age" => age}) do
+    Hello.Repo.insert(%Hello.Dog{name: name, age: age})
+    json(conn, %{status: "ok"})
+  end
 end
 
 require Protocol
