e291c9e92b784ebc1dee589a0917bfaf400deec3
Sergei Kukhariev <serge.kukharev@gmail.com>
2024-06-28 13:42:57 -0300

    Added random delay

--- phx-projects/downstream/lib/downstream_web/controllers/dog_controller.ex
+++ phx-projects/downstream/lib/downstream_web/controllers/dog_controller.ex
@@ -2,6 +2,7 @@ defmodule DownstreamWeb.DogController do
   use DownstreamWeb, :controller
 
   def index(conn, _) do
+    :timer.sleep(Enum.random(100..400))
     json(conn, %{test: true})
   end
 end
